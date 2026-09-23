461a55878e0c5885d0d281b99c9cace5a634fec2
Javier Bradlim <144743434+jbradlim08@users.noreply.github.com>
2026-09-23 15:44:34 -0500

    Fix HeadKnife system when CHARGE state. Instead of using a lot of conditions, I replace them with RayCast2D along with auxiliary function such as field_of_view(), player_detected(), and update_to_player_dir() to determine whether or not it can charge

--- enemy/ground/headknife.gd
+++ enemy/ground/headknife.gd
@@ -15,25 +15,26 @@ enum HKState{
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var right_detector: RayCast2D = $RightDetector
 @onready var left_detector: RayCast2D = $LeftDetector
+@onready var ground_detector: RayCast2D = $GroundDetector
+@onready var player_detector: RayCast2D = $PlayerDetector
 @onready var movement_timer: Timer = $MovementTimer
 
 @export var gravity_scale: float = 0.5
 @export var speed: float = 70.0
-@export var charge_speed: float = 100.0
+@export var charge_speed: float = 120.0
 @export var max_dist_x_to_player: float = 220.0
-@export var max_dist_y_to_player: float = 50.0
+@export var max_dist_y_to_player: float = 60.0
 
 var cur_state: HKState
 var dir: float = 0.0
+var to_player_dir: float = 0.0 # for charge purpose
 var state_velocity_x: float = 0.0
-var is_collide_wall: bool = false
 
 # can do state
 var can_patrol: bool = true
+var can_go_down: bool = true
 var can_attack: bool = true
 var can_hurt: bool = true
-
-var player_in_domain: bool = false
 
 
 func _ready() -> void:
@@ -44,67 +45,50 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	super(delta)
 	apply_gravity(delta)
+	check_all_detector()
+	update_to_player_dir()
+	update_player_detector()
 	
+	# check the state
+	check_state_movement()
+	check_state_fall()
+	
+	# the physics
 	handle_movement()
 	handle_facing()
 	handle_charge()
 	handle_attack()
 	
-	# check the state
-	check_side_wall()
-	check_movement()
-	check_fall()
-
 	move_and_slide()
 
 func apply_gravity(delta) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * gravity_scale * delta
 
-func check_movement() -> void:
-	if not is_on_floor() or can_charge():
+func check_state_movement() -> void:
+	if not is_on_floor():
 		return
 	if dir == 0.0:
 		set_state(HKState.IDLE)
 	else:
-		set_state(HKState.PATROL)
+		if not can_charge():
+			set_state(HKState.PATROL)
 
-func check_fall() -> void:
+func check_state_fall() -> void:
 	if not is_on_floor() and velocity.y >= 0:
 		set_state(HKState.FALL)
 
-func check_side_wall() -> void:
-	if not is_on_floor():
+
+func check_all_detector() -> void:
+	if not is_on_floor() or can_charge():
 		return
-	if right_detector.is_colliding():
-		if cur_state == HKState.CHARGE:
-			print('idle from side_wall check')
-			set_state(HKState.IDLE)
-			return
-		if cur_state == HKState.PATROL:
-			dir = -1.0
-	elif left_detector.is_colliding():
-		if cur_state == HKState.CHARGE:
-			print('idle from side_wall check')
-			set_state(HKState.IDLE)
-			return
-		if cur_state == HKState.PATROL:
-			dir = 1.0
-
-
-func can_charge() -> bool:
-	var charge: bool = false
-	if not player_in_domain or \
-	   (player_ref.is_on_floor() and \
-	   global_position.y - player_ref.global_position.y > max_dist_y_to_player) or \
-	   not is_on_floor() or \
-	   left_detector.is_colliding() or \
-	   right_detector.is_colliding():
-		charge = false
-	else:
-		charge = true
-	
-	return charge
+		
+	if right_detector.is_colliding() or \
+	   left_detector.is_colliding():
+			dir = -dir
+			
+	if not ground_detector.is_colliding():
+		dir = -dir
 
 func handle_movement() -> void:
 	velocity.x = dir * state_velocity_x
@@ -115,9 +99,11 @@ func handle_facing() -> void:
 	if dir > 0.0:
 		sprite.flip_h = false
 		hitbox_col.position.x = 17.5
+		ground_detector.position.x = 25
 	elif dir < 0.0:
 		sprite.flip_h = true
 		hitbox_col.position.x = -17.5
+		ground_detector.position.x = -25
 
 func handle_charge() -> void:
 	if can_charge():
@@ -129,6 +115,49 @@ func handle_charge() -> void:
 
 func handle_attack() -> void:
 	pass
+
+func update_to_player_dir() -> void:
+	if global_position.x <= player_ref.global_position.x:
+		to_player_dir = 1.0
+	else:
+		to_player_dir = -1.0
+
+func update_player_detector() -> void:
+	player_detector.rotation = global_position.angle_to_point(player_ref.global_position) \
+							   - deg_to_rad(90.0)
+
+func field_of_view() -> bool:
+	var horizon_dir: Vector2 = Vector2.RIGHT * to_player_dir
+	var to_player: Vector2 = global_position.direction_to(player_ref.global_position)
+	var angle: float = abs(horizon_dir.angle_to(to_player))
+	return angle <= deg_to_rad(18.0)
+	# minimum height for field of view and player detection to 'true'
+	# h = sin(18) * 200px(player detector length) = 60
+	# slightly lower than 2 tiles
+
+func player_detected() -> bool:
+	if not player_detector.is_colliding():
+		return false
+	else:
+		var collider = player_detector.get_collider()
+		return collider.is_in_group("player")
+
+func can_charge() -> bool:
+	var charge: bool
+	if not is_on_floor() or \
+	   not field_of_view() or \
+	   not player_detected():
+		charge = false
+	else:
+		charge = true
+	
+	if global_position.y > player_ref.global_position.y:
+		if global_position.y - player_ref.global_position.y <= max_dist_y_to_player and \
+		   player_ref.is_on_floor():
+			print(global_position.y, ", ", player_ref.global_position.y)
+			charge = true
+		
+	return charge
 
 #region State
 func set_state(new_state: HKState) -> void:
@@ -215,14 +244,10 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player_hurt"):
 		area.get_parent().take_damage(DataManager.get_dmg_default(), global_position)
 
-func _on_charge_domain_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player_body"):
-		player_in_domain = true
 
-func _on_charge_domain_body_exited(body: Node2D) -> void:
-	player_in_domain = false
-
+# interchange between IDLE and PATROL
 func _on_movement_timer_timeout() -> void:
+	print('take turn')
 	if cur_state == HKState.IDLE:
 		set_state(HKState.PATROL)
 		dir = 1.0 if randi_range(0, 1) == 0 else -1.0
